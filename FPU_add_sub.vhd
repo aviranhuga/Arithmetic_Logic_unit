ede20800c8f77abdbca47161d53c6ddae690732a
Aviran Huga <Aviranhuga@gmail.com>
2018-05-05 22:54:45 +0300

    finish FPU

--- FPU_add_sub.vhd
+++ FPU_add_sub.vhd
@@ -53,14 +53,8 @@ port (
 	add_sub_cout : in std_logic;
 	num : in std_logic_vector(23 downto 0);	
 	side_to_shift : out std_logic;
-	result : out std_logic_vector(4 downto 0)
-	);
-end component;
-
-component Rounding_unit
-port (
-	Input : in std_logic_vector(22 downto 0);
-	output : out std_logic_vector(22 downto 0)
+	result_f : out std_logic_vector(4 downto 0);
+	result_e : out std_logic_vector(7 downto 0)
 	);
 end component;
 
@@ -106,22 +100,27 @@ signal f2: std_logic_vector(23 downto 0);
 signal Exponent_Subtracter_result : std_logic_vector(7 downto 0);
 signal Alignment_Shifter_result : std_logic_vector(23 downto 0);
 signal Absoult_value_result : std_logic_vector(7 downto 0); 
-signal Significand_add_sub_result : std_logic_vector(23 downto 0);
-signal Normalize_output : std_logic_vector(22 downto 0);
-signal Significand_add_sub_result_AB : std_logic_vector(23 downto 0);
+signal Significand_add_sub_result : std_logic_vector(25 downto 0);
+signal Significand_add_sub_result_AB : std_logic_vector(25 downto 0);
 signal Significand_add_sub_cout : std_logic;
 signal Lead_zeros_counter_side : std_logic;
 signal max_exponent : std_logic_vector(15 downto 0);
 signal exponent_befor_bias : std_logic_vector(7 downto 0);
-signal Lead_zeros_counter_result : std_logic_vector(4 downto 0);
+signal Lead_zeros_counter_result_e : std_logic_vector(7 downto 0);
+signal Lead_zeros_counter_result_f : std_logic_vector(4 downto 0);
 signal Swap_o1 : std_logic_vector(23 downto 0);
 signal Swap_o2 : std_logic_vector(23 downto 0);
-
+signal Swap_add_sub_o1 : std_logic_vector(23 downto 0);
+signal Swap_add_sub_o2 : std_logic_vector(23 downto 0);
+signal Significand_add_sub_o1 : std_logic_vector(25 downto 0);
+signal Significand_add_sub_o2 : std_logic_vector(25 downto 0);
 
 signal one_signal: std_logic;
+signal one_vector: std_logic_vector(7 downto 0);
 signal zero_signal: std_logic;
 signal bias: std_logic_vector(7 downto 0);
 signal subtract_signal : std_logic;
+signal subtract_swap_sel : std_logic;
 
 begin
 e1 <= SIGNED(A(30 downto 23)) - SIGNED(bias);
@@ -133,12 +132,15 @@ f1(22 downto 0) <= A(22 downto 0);
 f2(23) <= '1';
 f2(22 downto 0) <= B(22 downto 0);
 one_signal <= '1';
+one_vector <= "00000001";
 zero_signal <= '0';
 bias <= "01111111";
 subtract_signal <= s1 xor s2; --for Significand_add_sub
-			  
-
-			  
+subtract_swap_sel <= s1 and (not Exponent_Subtracter_result(7)) and subtract_signal;
+Significand_add_sub_o1(25 downto 24) <= "00";
+Significand_add_sub_o1(23 downto 0) <= Swap_add_sub_o1;
+Significand_add_sub_o2(25 downto 24) <= "00";
+Significand_add_sub_o2(23 downto 0) <= Swap_add_sub_o2;
 --component init
 
 	Exponent_Subtracter: add_sub_N
@@ -172,27 +174,36 @@ subtract_signal <= s1 xor s2; --for Significand_add_sub
 		left_side => zero_signal,
 		result => Alignment_Shifter_result);
 		
-	Significand_add_sub: add_sub_N
-		generic map (N => 24)
+	Swap_for_add_sub: Swap_FPU
 		port map(
-		x => Swap_o1,
-		y => Alignment_Shifter_result,
+		f1 => f1,
+		f2 => Alignment_Shifter_result,
+		SEL => subtract_swap_sel,
+		o1 => Swap_add_sub_o1,
+		o2 => Swap_add_sub_o2);	
+		
+	Significand_add_sub: add_sub_N
+		generic map (N => 26)
+		port map(
+		x => Significand_add_sub_o1,
+		y => Significand_add_sub_o2,
 		sub => subtract_signal,
 		cout => Significand_add_sub_cout,
 		result => Significand_add_sub_result);
 		
 	Significand_add_sub_AB: Absoult_value
-		generic map(N => 24)
+		generic map(N => 26)
 		port map(
 		Input => Significand_add_sub_result,
 		Output => Significand_add_sub_result_AB);
 		
 	Lead_zeros_Counter_s: Lead_zeros_counter 
 		port map (
-		add_sub_cout =>Significand_add_sub_cout,
-		num => Significand_add_sub_result_AB,
+		add_sub_cout =>Significand_add_sub_result_AB(24),
+		num => Significand_add_sub_result_AB(23 downto 0),
 		side_to_shift => Lead_zeros_counter_side,
-		result => Lead_zeros_counter_result);
+		result_e => Lead_zeros_counter_result_e,
+		result_f => Lead_zeros_counter_result_f);
 		
 	Select_exponent : min_max
 		generic map(N => 8)
@@ -207,28 +218,21 @@ subtract_signal <= s1 xor s2; --for Significand_add_sub
 		generic map (N => 23)
 		port map(
 		a => Significand_add_sub_result_AB(22 downto 0),
-		b => Lead_zeros_counter_result,
+		b => Lead_zeros_counter_result_f,
 		left_side => Lead_zeros_counter_side,
-		result => Normalize_output);
-
-		Rounding_unit_output: Rounding_unit
-		port map(
-		Input => Normalize_output,
-		output => result(22 downto 0));
+		result => result(22 downto 0));
 		
-		exponent_result_shift: shift_unit
+		exponent_result_shift: add_sub_N
 		generic map (N => 8)
 		port map(
-		a => max_exponent(7 downto 0),
-		b => Lead_zeros_counter_result,
-		left_side => zero_signal,
+		x => max_exponent(7 downto 0),
+		y => Lead_zeros_counter_result_e,
+		sub => Lead_zeros_counter_side,
+		cout => open,
 		result => exponent_befor_bias);
-
-		result(31) <= (
-			  (s2 and Significand_add_sub_result(22)) or
-			  (s2 and Exponent_Subtracter_result(7)) or
-			  ((not Exponent_Subtracter_result(7)) and (not Significand_add_sub_result(22)) and s1));
-			  
+		
+		
+		result(31) <= Significand_add_sub_result(25) or (s1 and s2);
 		result(30 downto 23) <= SIGNED(exponent_befor_bias) + SIGNED(bias);
 	
 end FPU_add_sub_arch;
--- Lead_zeros_counter.vhd
+++ Lead_zeros_counter.vhd
@@ -20,7 +20,8 @@ port (
 	add_sub_cout : in std_logic;
 	num : in std_logic_vector(23 downto 0);	
 	side_to_shift : out std_logic;
-	result : out std_logic_vector(4 downto 0)
+	result_f : out std_logic_vector(4 downto 0);
+	result_e : out std_logic_vector(7 downto 0)
 	);
 end Lead_zeros_counter;
 
@@ -32,7 +33,8 @@ begin
 	variable leading_zeros : integer;
 	begin
 		if add_sub_cout='1' then
-			result <= "00000";
+			result_e <= "00000001";
+			result_f <= "00001";
 		else
 		leading_zeros := 0;
 		for i in 23 downto 0 loop
@@ -41,7 +43,8 @@ begin
 			end if;
 			leading_zeros := leading_zeros + 1;
 		end loop;
-		 result <= std_logic_vector(to_unsigned(leading_zeros,5));
+		 result_e <= std_logic_vector(to_unsigned(leading_zeros,8));
+		 result_f <= std_logic_vector(to_unsigned(leading_zeros,5));
 		end if;
 	end process;
 end Lead_zeros_counter_arch;
